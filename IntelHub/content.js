febf8e4c570dfabbdb82d0de4c259195bc19754e
TomSec <43483906+tomsec8@users.noreply.github.com>
2025-11-25 16:50:43 +0200

    Delete IntelHub directory

--- IntelHub/content.js
+++
@@ -1,6 +0,0 @@
-chrome.runtime.onMessage.addListener((msg, sender, sendResponse) => {
-  if (msg.action === "extractPageText") {
-    const text = document.body ? document.body.innerText : "";
-    sendResponse({ text });
-  }
-});
